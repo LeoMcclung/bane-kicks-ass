6d3018dba6bb1d1d4ad4500f9dbea71c58cc11ca
Leo McClung <murraythemoose@gmail.com>
2017-08-06 14:37:34 -0600

    madlibs and images

---
+++ public/bane-madlibs/bane-madlibs.js
@@ -0,0 +1,29 @@
+function addWordsToMadlib() {
+
+    var word1 = document.querySelector("#word-input-1").value;
+    var word2 = document.querySelector("#word-input-2").value;
+    var word3 = document.querySelector("#word-input-3").value;
+    var word4 = document.querySelector("#word-input-4").value;
+    var word5 = document.querySelector("#word-input-5").value;
+    var word6 = document.querySelector("#word-input-6").value;
+    var word7 = document.querySelector("#word-input-7").value;
+    var word8 = document.querySelector("#word-input-8").value;
+    var word9 = document.querySelector("#word-input-9").value;
+    var word10 = document.querySelector("#word-input-10").value;
+    var word11 = document.querySelector("#word-input-11").value;
+    var word12 = document.querySelector("#word-input-12").value;
+
+    document.querySelector("#word-1").textContent = word1;
+    document.querySelector("#word-2").textContent = word2;
+    document.querySelector("#word-3").textContent = word3;
+    document.querySelector("#word-4").textContent = word4;
+    document.querySelector("#word-5").textContent = word5;
+    document.querySelector("#word-6").textContent = word6;
+    document.querySelector("#word-7").textContent = word7;
+    document.querySelector("#word-8").textContent = word8;
+    document.querySelector("#word-9").textContent = word9;
+    document.querySelector("#word-10").textContent = word10;
+    document.querySelector("#word-11").textContent = word11;
+    document.querySelector("#word-12").textContent = word12;
+
+}
